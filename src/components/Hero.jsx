d0f2fccae180fbc4d3ccf4c05b0b856cd567d2ad
Fanajaval <andriatoavimanana@mail.com>
2026-07-10 18:43:40 +0300

    Initialisation du portfolio

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import "../styles/Hero.css"
+
+function Hero(){
+  return(
+    <section className="hero">
+      <div className="hero-content">
+        <h1>Bonjour, je suis Fanajarilala</h1>
+        <h2>Développeur Full Stack en apprentissage</h2>
+
+        <p>Je crée des applications web modernes avec React, Node.js et les technologies du web.</p>
+
+        <button>Me contacter</button>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
