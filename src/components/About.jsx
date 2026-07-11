6aacade42c43047cdd9cc464be8289afbcf39adb
Fanajaval <andriatoavimanana@gmail.com>
2026-07-11 13:10:22 +0300

    ajout about

---
+++ src/components/About.jsx
@@ -0,0 +1,34 @@
+import "../styles/About.css";
+
+function About() {
+  return (
+    <section className="about" id="about">
+
+      <div className="about-container">
+
+        <h2>
+          À propos de moi
+        </h2>
+
+        <p>
+          Je suis Fanajarilala Valério, étudiant en troisième année
+          en Génie Logiciel et Base de Données à l'ENI Fianarantsoa.
+        </p>
+
+        <p>
+          Passionné par le développement web, je crée des applications
+          modernes avec React, Node.js et les technologies du web.
+        </p>
+
+        <p>
+          Mon objectif est de continuer à progresser et de participer
+          à des projets innovants.
+        </p>
+
+      </div>
+
+    </section>
+  );
+}
+
+export default About;
