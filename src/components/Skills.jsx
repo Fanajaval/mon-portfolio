cbaec3bf4b6f0c5fa0d599d228299f4b3ee4f79d
Fanajaval <andriatoavimanana@gmail.com>
2026-07-18 13:50:20 +0300

    Ajout compétences

---
+++ src/components/Skills.jsx
@@ -0,0 +1,43 @@
+import "../styles/Skills.css";
+
+function Skills() {
+  return (
+    <section className="skills" id="skills">
+
+      <h2 className="section-title">
+        Mes Compétences
+      </h2>
+
+      <div className="skills-grid">
+
+        <div className="skill-card">
+          <h3>Java</h3>
+        </div>
+
+        <div className="skill-card">
+          <h3>Python</h3>
+        </div>
+
+        <div className="skill-card">
+          <h3>JavaScript</h3>
+        </div>
+
+        <div className="skill-card">
+          <h3>PHP</h3>
+        </div>
+
+        <div className="skill-card">
+          <h3>React</h3>
+        </div>
+
+        <div className="skill-card">
+          <h3>Node.js</h3>
+        </div>
+
+      </div>
+
+    </section>
+  );
+}
+
+export default Skills;
