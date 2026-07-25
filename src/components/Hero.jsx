4ab02bd518767ff59cdcc1c1141a5f28035d099a
Fanajaval <andriatoavimanana@gmail.com>
2026-07-25 10:37:15 +0300

    amélioration animation boutons dans hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -54,12 +54,14 @@ function Hero() {
 
         <div className="hero-buttons">
           <motion.a href="#contact" className="btn-primary" 
+          whileHover={{scale: 1.1}}
           whileTap={{ scale: 0.9 }} 
           transition={{duration: 0.1}}>
             Me contacter
           </motion.a>
 
           <motion.a href="/cv.pdf" className="btn-secondary" download 
+          whileHover={{scale: 1.1}}
           whileTap={{ scale: 0.9 }} 
           transition={{duration: 0.1}}>
             Télécharger mon CV
