f54b48867165e4e88e52f34726554ea5d9993d63
Fanajaval <andriatoavimanana@gmail.com>
2026-08-10 16:40:37 +0300

    dark mode section navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,33 +1,99 @@
 import { useState } from "react";
+
+import {
+  FaMoon,
+  FaSun,
+} from "react-icons/fa6";
+
 import "../styles/Navbar.css";
 
-function Navbar() {
+function Navbar({ theme, toggleTheme }) {
+
   const [menuOpen, setMenuOpen] = useState(false);
 
   return (
-    <header className="header">
-      <nav className="navbar">
-        <div className="logo">FV</div>
 
-        <button
-          className="menu-btn"
-          onClick={() => setMenuOpen(!menuOpen)}
-          aria-label="Ouvrir le menu"
-        >
-          {menuOpen ? "✕" : "☰"}
-        </button>
+    <header className="header">
+
+      <nav className="navbar">
+
+        <a href="#hero" className="logo">
+          FV
+        </a>
+
+
+        <div className="nav-actions">
+            <button
+                className="theme-btn"
+                onClick={toggleTheme}
+                aria-label="Changer le thème"
+            >
+                {theme === "light"
+                ? <FaMoon />
+                : <FaSun />}
+            </button>
+
+            <button
+                className="menu-btn"
+                onClick={() => setMenuOpen(!menuOpen)}
+                aria-label="Ouvrir le menu"
+            >
+                {menuOpen ? "✕" : "☰"}
+            </button>
+
+            </div>
+
 
         <ul className={`nav-links ${menuOpen ? "open" : ""}`}>
-          <li><a href="#hero" onClick={() => setMenuOpen(false)}>Accueil</a></li>
-          <li><a href="#about" onClick={() => setMenuOpen(false)}>À propos</a></li>
-          <li><a href="#skills" onClick={() => setMenuOpen(false)}>Compétences</a></li>
-          <li><a href="#projects" onClick={() => setMenuOpen(false)}>Projets</a></li>
-          <li><a href="#education" onClick={() => setMenuOpen(false)}>Éducation</a></li>
-          <li><a href="#certifications" onClick={() => setMenuOpen(false)}>Certifications</a></li>
-          <li><a href="#contact" onClick={() => setMenuOpen(false)}>Contact</a></li>
+
+          <li>
+            <a href="#hero" onClick={() => setMenuOpen(false)}>
+              Accueil
+            </a>
+          </li>
+
+          <li>
+            <a href="#about" onClick={() => setMenuOpen(false)}>
+              À propos
+            </a>
+          </li>
+
+          <li>
+            <a href="#skills" onClick={() => setMenuOpen(false)}>
+              Compétences
+            </a>
+          </li>
+
+          <li>
+            <a href="#projects" onClick={() => setMenuOpen(false)}>
+              Projets
+            </a>
+          </li>
+
+          <li>
+            <a href="#education" onClick={() => setMenuOpen(false)}>
+              Formation
+            </a>
+          </li>
+
+          <li>
+            <a href="#certifications" onClick={() => setMenuOpen(false)}>
+              Certifications
+            </a>
+          </li>
+
+          <li>
+            <a href="#contact" onClick={() => setMenuOpen(false)}>
+              Contact
+            </a>
+          </li>
+
         </ul>
+
       </nav>
+
     </header>
+
   );
 }
 
